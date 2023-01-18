86754b462b92b7f8c04497a3c73caebfec640726
Augustin <augustin.affognon@etu.uca.fr>
2023-01-18 10:12:53 +0100

    Second

---
+++ Tests/BOARDTests/BOARDTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BOARD
+
+final class BOARDTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BOARD().text, "Hello, World!")
+    }
+}
